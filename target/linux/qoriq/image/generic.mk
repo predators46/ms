21f35cf3307000e6d5b0b082e473735d16c8fe7e
Stijn Tintel <stijn@linux-ipv6.be>
2021-12-06 12:45:12 +0200

    qoriq: add support for WatchGuard Firebox M200
    Boots, no network.
    
    setenv OpenWrt_fdt image-watchguard-firebox-m200.dtb
    setenv OpenWrt_kernel watchguard_firebox-m200-kernel.bin
    setenv wgBootSysA 'setenv bootargs root=/dev/mmcblk0p2 rw rootdelay=2 console=$consoledev,$baudrate fsl_dpaa_fman.fsl_fm_max_frm=1530; ext2load mmc 0:1 $fdtaddr $OpenWrt_fdt; ext2load mmc 0:1 $loadaddr $OpenWrt_kernel; bootm $loadaddr - $fdtaddr'
    saveenv
    reset
    
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- target/linux/qoriq/image/generic.mk
+++ target/linux/qoriq/image/generic.mk
@@ -1,3 +1,18 @@
+define Device/watchguard_firebox-m200
+  DEVICE_VENDOR := WatchGuard
+  DEVICE_MODEL := Firebox M200
+  DEVICE_DTS_DIR := $(DTS_DIR)/fsl
+  DEVICE_PACKAGES := \
+	kmod-hwmon-w83793 kmod-ptp-qoriq kmod-rtc-rs5c372a kmod-tpm-i2c-atmel
+  BLOCKSIZE := 128k
+  KERNEL := kernel-bin | gzip | uImage gzip
+  IMAGES := sdcard.img.gz sysupgrade.img.gz
+  IMAGE/sysupgrade.img.gz :=  sdcard-img | gzip | append-metadata
+  IMAGE/sdcard.img.gz := sdcard-img | gzip
+endef
+
+TARGET_DEVICES += watchguard_firebox-m200
+
 define Device/watchguard_firebox-m300
   DEVICE_VENDOR := WatchGuard
   DEVICE_MODEL := Firebox M300
